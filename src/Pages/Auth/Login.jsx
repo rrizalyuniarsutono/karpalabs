ab921effd0654cd5d087e330a8b43379832fa2c3
unknown <rizalyuniar123@gmail.com>
2023-08-26 15:53:00 +0700

    darkmode

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../../assets/img/logo.png";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
+import style from "./style.module.css"
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -27,59 +28,84 @@ const Login = () => {
       });
     }
   };
+  // dark mode
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const preferredMode = localStorage.getItem('preferredMode');
+    if (preferredMode === 'dark') {
+      setIsDarkMode(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add('dark-mode');
+      localStorage.setItem('preferredMode', 'dark');
+    } else {
+      document.body.classList.remove('dark-mode');
+      localStorage.setItem('preferredMode', 'light');
+    }
+  }, [isDarkMode]);
 
   return (
-    <div
-      className="card shadow"
-      style={{
-        border: "white",
-        width: "400px",
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        borderBottomLeftRadius: 20,
-        borderBottomRightRadius: 20,
-      }}
-    >
-      <div className="align-items-center mt-1">
-        <img
-          src={Logo}
-          style={{
-            width: 120,
-            height: 120,
-            borderRadius: "50%",
-          }}
-          className="card-img-top mt-2"
-          alt="..."
-        />
-      </div>
-      <div className="card-body">
-        <form onSubmit={handleLogin}>
-          <div className="form-group">
-            <label className="d-flex flex-row">Username</label>
-            <input
-              type="text"
-              className="form-control"
-              id="exampleInputEmail1"
-              placeholder="Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </div>
-          <div className="form-group mt-2">
-            <label className="d-flex flex-row">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary mt-3" style={{ width: "300px" }}>
-            SIGN IN
-          </button>
-        </form>
+    <div style={{
+      position: "absolute",
+      right: "35%",
+      top: "15%"
+    }}>
+      <div
+        className={`card shadow ${style.card} ${isDarkMode ? style.dark : ""}`}
+        style={{
+          border: "white",
+          width: "400px",
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          borderBottomLeftRadius: 20,
+          borderBottomRightRadius: 20,
+        }}
+      >
+        <div className="mx-auto mt-1">
+          <img
+            src={Logo}
+            style={{
+              width: 120,
+              height: 120,
+              borderRadius: "50%",
+            }}
+            className="card-img-top mt-2"
+            alt="..."
+          />
+        </div>
+        <div className="card-body">
+          <form onSubmit={handleLogin}>
+            <div className="form-group">
+              <label className="d-flex flex-row">Username</label>
+              <input
+                type="text"
+                className="form-control"
+                id="exampleInputEmail1"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="form-group mt-2">
+              <label className="d-flex flex-row">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit" className={`btn mt-3 btn-primary ${style.button} ${isDarkMode ? style.dark : ""}`} style={{ width: "300px" }}>
+              SIGN IN
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
